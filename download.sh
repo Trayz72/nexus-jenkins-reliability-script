03c3f0da0757a067c59a0db6e9cb7b1a3e746350
Trayz72 <shahtanishq72@gmail.com>
2026-02-23 11:33:47 +0530

    Improved sorting logic for latest build pull

--- download.sh
+++ download.sh
@@ -9,11 +9,17 @@ echo "Fetching latest artifacts from Nexus at ${NEXUS_IP}..."
 
 mkdir -p /root/app_deploy && cd /root/app_deploy
 
-BACKEND_URL=$(curl -u "$USER:$PASS" -s -X GET "${URL}?repository=spring-boot-repo&maven.extension=jar&sort=version&direction=desc" \
-| grep -Po '"downloadUrl" : "\K[^"]*' | head -1)
+BACKEND_URL=$(curl -u "$USER:$PASS" -s -X GET \
+"${URL}?repository=spring-boot-repo&maven.extension=jar" \
+| grep -Po '"downloadUrl" : "\K[^"]*' \
+| sort -V \
+| tail -1)
 
-FRONTEND_URL=$(curl -u "$USER:$PASS" -s -X GET "${URL}?repository=angular-app-repo&sort=version&direction=desc" \
-| grep -Po '"downloadUrl" : "\K[^"]*' | head -1)
+FRONTEND_URL=$(curl -u "$USER:$PASS" -s -X GET \
+"${URL}?repository=angular-app-repo" \
+| grep -Po '"downloadUrl" : "\K[^"]*' \
+| sort -V \
+| tail -1)
 
 echo "Downloading Backend: $BACKEND_URL"
 curl -u "$USER:$PASS" -L "$BACKEND_URL" -o backend.jar
